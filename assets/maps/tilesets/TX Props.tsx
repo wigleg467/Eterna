eb7274627c3fc427cafc46aa3df5cbe9c188420c
olyaaaa <olechka.tkachukk@gmail.com>
2025-06-10 00:27:58 +0300

    map collisions

--- assets/maps/tilesets/TX Props.tsx
+++ assets/maps/tilesets/TX Props.tsx
@@ -1,5 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TX Props" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="../images/TX Props.png" width="512" height="512"/>
+ <tile id="57">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="1.72332" y="10.5532" width="3.28022" height="16.3241"/>
+   <object id="4" x="0" y="21.229" width="32" height="5.77097"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="26.9351" y="9.69418" width="4.88084" height="16.7536"/>
+   <object id="2" x="0" y="20.9836" width="32" height="6.01639"/>
+  </objectgroup>
+ </tile>
  <tile id="247" probability="0.3"/>
 </tileset>
