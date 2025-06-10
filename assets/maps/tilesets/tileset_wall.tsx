c0bc056bfdb5bd3ec2c5c1896b0f45af32acc8ae
olyaaaa <olechka.tkachukk@gmail.com>
2025-06-10 14:15:38 +0300

    Add walls collisions blocked property

--- assets/maps/tilesets/tileset_wall.tsx
+++ assets/maps/tilesets/tileset_wall.tsx
@@ -2,33 +2,76 @@
 <tileset version="1.10" tiledversion="1.11.2" name="tileset_wall" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="../images/TX Tileset Wall.png" width="512" height="512"/>
  <tile id="25">
+  <properties>
+   <property name="blocked" type="bool" value="false"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0.454545" y="-0.0909091" width="7.36364" height="32"/>
   </objectgroup>
  </tile>
  <tile id="26">
+  <properties>
+   <property name="blocked" type="bool" value="false"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="24" y="0" width="8" height="32"/>
   </objectgroup>
  </tile>
+ <tile id="41">
+  <properties>
+   <property name="blocked" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="blocked" type="bool" value="false"/>
+  </properties>
+ </tile>
  <tile id="57">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0.368133" y="0.245422">
-    <polygon points="0,0 0.184067,31.1073 0.368133,31.7208 1.22711,31.7208 1.04304,-0.0613556 0.981689,-0.429489"/>
+    <polygon points="0,0 1.00907,31.1073 2.01813,31.7208 6.72711,31.7208 5.71802,-0.0613556 5.38169,-0.429489"/>
    </object>
   </objectgroup>
  </tile>
+ <tile id="58">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="97">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0.398236" y="0.0613556" width="4.66667" height="31.9386"/>
   </objectgroup>
  </tile>
+ <tile id="98">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="100">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="24.3582" y="0.0613556" width="6.66667" height="32"/>
   </objectgroup>
  </tile>
  <tile id="113">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="26.5056" width="32" height="5.49439"/>
    <object id="2" x="0" y="22.7016" width="32" height="9.29844"/>
@@ -36,16 +79,25 @@
   </objectgroup>
  </tile>
  <tile id="114">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="25.7693" width="32" height="6.23066"/>
   </objectgroup>
  </tile>
  <tile id="115">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="22.7016" width="32" height="9.29844"/>
   </objectgroup>
  </tile>
  <tile id="116">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="22.0267" width="32" height="9.97335"/>
    <object id="2" x="28.1622" y="0.245422" width="3.66667" height="32"/>
